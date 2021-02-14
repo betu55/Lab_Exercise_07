c364a3f1435316e5fb0273e508d53792c5797c99
Betu55 <se.bemenet.semegnew@gmail.com>
2021-02-14 18:51:56 +0300

    The analog clock is fully functional and ready to go.

---
+++ AsyncDemo/Assets/js/clock.js
@@ -0,0 +1,19 @@
+const deg = 6;
+const hr = document.querySelector('#hr');
+const min = document.querySelector('#min');
+const sec = document.querySelector('#sec');
+
+function rotation() {
+    let currentTime = new Date();
+    let hour = currentTime.getHours() * 30;
+    let minute = currentTime.getMinutes() * deg;
+    let second = currentTime.getSeconds() * deg;
+
+    hr.style.transform = `rotateZ(${hour}deg)`;
+    min.style.transform = `rotateZ(${minute}deg)`;
+    sec.style.transform = `rotateZ(${second}deg)`;
+
+}
+
+setInterval(rotation, 1000);
+
